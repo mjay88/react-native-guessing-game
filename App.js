6bfe0f457ad507e50a52aa96bd6cc5105b090533
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-03-06 00:26:06 -0600

    background image, gradient, custom button in  PrimaryButton.jsx

--- App.js
+++ App.js
@@ -1,9 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, ImageBackground } from "react-native";
 import StartGameScreen from "./screens/StartGameScreen";
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function App() {
-	return <StartGameScreen></StartGameScreen>;
+	//views only take up as much space as they need to fit the content inside of them
+	return (
+		<LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.rootScreen}>
+			<ImageBackground
+				source={require("./assets/images/background.png")}
+				resizeMode="cover"
+				style={styles.rootScreen}
+				imageStyle={styles.backgroundImage}
+			>
+				<StartGameScreen />
+			</ImageBackground>
+		</LinearGradient>
+	);
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	rootScreen: {
+		flex: 1,
+	},
+	backgroundImage: {
+		opacity: 0.15,
+	},
+});
--- screens/StartGameScreen.jsx
+++ screens/StartGameScreen.jsx
@@ -1,7 +1,30 @@
-import { TextInput, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { TextInput, View, StyleSheet, Alert } from "react-native";
 import PrimaryButton from "../components/PrimaryButton";
 
 export default function StartGameScreen() {
+	const [enteredNumber, setEnteredNumber] = useState("");
+
+	function numberInputHandler(enteredText) {
+		setEnteredNumber(enteredText);
+	}
+
+	function resetInputHandler() {
+		setEnteredNumber("");
+	}
+
+	function confirmInputHandler() {
+		const chosenNumber = parseInt(enteredNumber);
+
+		if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+			Alert.alert(
+				"Invalid number!",
+				"Number had to be a number between 1 and 99",
+				[{ text: "Okay", style: "destructive", onPress: resetInputHandler }]
+			);
+			return;
+		}
+	}
 	return (
 		<View style={styles.inputContainer}>
 			<TextInput
@@ -10,19 +33,29 @@ export default function StartGameScreen() {
 				keyboardType="number-pad"
 				autoCapitalize="none"
 				autoCorrect={false}
+				onChangeText={numberInputHandler}
+				value={enteredNumber}
 			></TextInput>
-			<PrimaryButton>Reset</PrimaryButton>
-			<PrimaryButton>Confirm</PrimaryButton>
+			<View style={styles.buttonsContainer}>
+				<View style={styles.buttonContainer}>
+					<PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+				</View>
+				<View style={styles.buttonContainer}>
+					<PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+				</View>
+			</View>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	inputContainer: {
+		justifyContent: "center",
+		alignItems: "center",
 		marginTop: 100,
 		marginHorizontal: 24,
 		padding: 16,
-		backgroundColor: "#72063c",
+		backgroundColor: "#3b021f",
 		borderRadius: 8,
 		elevation: 4, //android only
 		//shadow property is IOS only
@@ -41,5 +74,11 @@ const styles = StyleSheet.create({
 		marginVertical: 8,
 		fontWeight: "bold",
 		textAlign: "center",
+	},
+	buttonsContainer: {
+		flexDirection: "row",
+	},
+	buttonContainer: {
+		flex: 1,
 	},
 });
